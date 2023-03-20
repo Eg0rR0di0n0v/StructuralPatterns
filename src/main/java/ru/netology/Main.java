15c61fc8d60e4968be4f9ba7f8c08ee538b8bde8
Eg0rR0di0n0v <Eg0rR0di0n0v@yandex.ru>
2023-03-20 12:12:55 +0300

    first commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        Ints intsCalc = new IntsCalculator();
+        //демонстрация
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+
+    }
+}
